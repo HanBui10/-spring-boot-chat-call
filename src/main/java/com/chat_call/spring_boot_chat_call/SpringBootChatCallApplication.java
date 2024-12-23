eb9b6d2b78b1a3c4c897d624ed5e7b7af6962649
HanBui10 <buih72436@gmail.com>
2024-12-23 20:01:52 +0700

    Initial commit

---
+++ src/main/java/com/chat_call/spring_boot_chat_call/SpringBootChatCallApplication.java
@@ -0,0 +1,13 @@
+package com.chat_call.spring_boot_chat_call;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat_call/spring_boot_chat_call/SpringBootChatCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_call.spring_boot_chat_call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
